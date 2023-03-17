727dd1e90c8a24113e5846e496f0dc70fd1ccf56
Nithish Krishna <105929790+nithishkrishna21@users.noreply.github.com>
2023-03-17 13:04:25 +0530

    Update app.js
    from localhost to 127.0.0.1

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const _ = require("lodash");
 
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
-mongoose.connect("mongodb://localhost:27017/blogDB");
+mongoose.connect("mongodb://127.0.0.1:27017/blogDB");
+
+// console.log("works till here");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -34,8 +36,10 @@ app.get("/", function(req, res){
   // find the composed posts and send them along with homeStartingContent
   Post.find(function(err,foundPosts){
     // if there are no errors
-    if(!err)
+    if(!err){
+      // console.log("Connected");
       res.render("home", {startingContent: homeStartingContent, posts: foundPosts});
+    }
     else
       console.log(err);
   });
